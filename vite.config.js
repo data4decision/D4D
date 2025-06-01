dc6fbd283fbaf90c1c3cecc6b774bad56433f23e
Oluwakayode <adegbesanoluwakayode52@gmail.com>
2025-06-01 16:20:17 +0100

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,23 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-// import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// // https://vite.dev/config/
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  base: "/D4D/",
+});
+
+// import { defineConfig } from "vite";
+// import tailwindcss from "@tailwindcss/vite";
 // export default defineConfig({
-//   plugins: [react(), tailwindcss()],
+//   plugins: [tailwindcss()],
 //   base: "/D4D/",
 // });
 
-// // import { defineConfig } from "vite";
-// // import tailwindcss from "@tailwindcss/vite";
-// // export default defineConfig({
-// //   plugins: [tailwindcss()],
-// //   base: "/D4D/",
-// // });
-
-export default {
-  base: "/D4D/",
-  build: {
-    chunkSizeWarningLimit: 600,
-  },
-};
+// export default {
+//   base: "/D4D/",
+//   build: {
+//     chunkSizeWarningLimit: 600,
+//   },
+// };
